fd7ff95f8b3690a289c41f92b6e4aa61be43912b
sanchay <sanchayshan@gmail.com>
2025-07-24 00:29:43 +0530

    Added Favorite icon

--- src/Card.js
+++ src/Card.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import './Card.css'
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
-function Card({src,title,description,price}) {
+
+function Card({ src, title, description, price }) {
   return (
-    <div className ='card'>
-        <img src={src} alt=""/>
-        <div className ="card__info">
-            <h2>{title}</h2>
-            <h4>{description}</h4>
-            <h3>{price}</h3>
-        </div>
+    <div className='card'>
+      <div className="card__imageContainer">
+        <img src={src} alt="" />
+        <FavoriteBorderIcon className="card__heartIcon" />
+      </div>
 
-      
+      <div className="card__info">
+        <h2>{title}</h2>
+        <h4>{description}</h4>
+        <h3>{price}</h3>
+      </div>
+
+
     </div>
   )
 }
--- src/Home.js
+++ src/Home.js
@@ -59,6 +59,9 @@ function Home() {
           <Card src="https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
           title="Rooms"
           description ="Unique activities we can do together, led by a world of hosts"/>
+          <Card src="https://plus.unsplash.com/premium_photo-1689609950112-d66095626efb?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          title="Rooms"
+          description ="Unique activities we can do together, led by a world of hosts"/>
 
 
 
